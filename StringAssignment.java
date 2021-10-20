5476e7dfba642bf25c91f1406f12a7ff6aaf3839
RabeccaMary <91882311+RabeccaMary@users.noreply.github.com>
2021-10-20 17:14:40 +0530

    Add files via upload

---
+++ StringAssignment.java
@@ -0,0 +1,31 @@
+package week3.day1;
+
+public class StringAssignment {
+
+	public static void main(String[] args) {
+		String text = "Java Exercise";
+		System.out.println(text.indexOf('s'));
+		String s1 = "I am Learning Java";
+		String s2 = "I am learning java";
+		if (s1 == s2)
+			System.out.println("same");
+		else
+			System.out.println("Not same");
+		if (s1.equals(s2))
+			System.out.println("same");
+		else
+			System.out.println("Not same");
+		if (s1.equalsIgnoreCase(s2))
+			System.out.println("same");
+		else
+			System.out.println("Not same");
+		String str1 = new String("Kutty");
+		String str2 = new String("Kutty");
+		if (str1.equals(str2))
+			System.out.println("same");
+		else
+			System.out.println("Not same");
+
+	}
+
+}
